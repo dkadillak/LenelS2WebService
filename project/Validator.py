5266d2ba0e251eb6496f029281f3c2058f41ce4a
Devin Kadillak <devbot@rams.colostate.edu>
2018-12-18 17:17:54 -0700

    almost everything implemented

--- project/Validator.py
+++ project/Validator.py
@@ -23,14 +23,46 @@ class Validator:
                 return False
         return True
 
+    def does_id_exist(self, person_id):
+        # making sure what's passed in isn't an int
+        person_id = str(person_id)
+        for x in range(0, self.person_list.__len__()):
+            if self.person_list[x].info["id"] == person_id:
+                return x
+
+        return -1
+
+    def does_filter_match(self, list_filter, index):
+        if self.person_list[index].info["first_name"] == list_filter:
+            return True
+        elif self.person_list[index].info["last_name"] == list_filter:
+            return True
+        else:
+            return False
+
+    def create_list_by_filter(self, list_filter):
+        list_filter = str(list_filter)
+        matches = []
+        for index in range(0, self.person_list.__len__()):
+            if self.does_filter_match(list_filter, index):
+                matches.append(self.person_list[index])
+
+        return matches
 
 def main():
     p = Person(1, "a", "b")
-    p1 = Person(2, "a", "b")
+    p1 = Person(2, "c", "a")
     v = Validator()
     print(v.add_person(p))
     print(v.add_person(p1))
-    print(v.person_list)
+    # print(v.person_list)
+
+    print(v.create_list_by_filter("c"))
+
+
+
+
+
 
 
 if __name__ == '__main__':
