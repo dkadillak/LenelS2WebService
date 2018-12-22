5d46f9dae4af4e273c08769913319c6c615df055
Devin Kadillak <devbot@rams.colostate.edu>
2018-12-22 00:13:17 -0700

    Just need to verify GET by filter is correct

--- project/Validator.py
+++ project/Validator.py
@@ -10,45 +10,40 @@ class Validator:
         self.id_list = []   # id list is for quickly answering questions about id's being unique/existing
 
     def add_person(self, person):
+        self.person_list.append(person)
+        self.id_list.append(person.info["id"])
 
-        if self.is_id_unique(person):
-            self.person_list.append(person)
-            self.id_list.append(person.info["id"])
-            return True
-        else:
-            return False
-
-    def is_id_valid(self, person_id):
-        if isinstance(person_id, str):
-            return "str_id"
-
+    # check if id is integer
+    def validate_id(self, person_id):
         try:
             person_id = int(person_id)
         except ValueError:
-            return "float_id"
+            return "str_id"
 
         if person_id <= 0:
             return "inv_id_range"
 
-        return person_id
+        return True
 
-    def is_id_unique(self, person):
-
-        if person.info["id"] in self.id_list:
-            return False
-        else:
-            return True
+    def remove_person(self, index):
+        # update both the person list and id list
+        self.person_list.remove(self.person_list[index])
+        self.id_list.remove(self.id_list[index])
 
     def does_id_exist(self, person_id):
-
-        # making sure what's passed in isn't an int
-        person_id = int(person_id)
-
-        try:
-            index = self.id_list.index(person_id)
-            return index
-        except ValueError:
-            return -1
+        # first validate id
+        status = self.validate_id(person_id)
+        if isinstance(status, str):
+            return status
+        else:
+            # once id is valid, see if a person exists with that id
+            try:
+                # id exists
+                index = self.id_list.index(int(person_id))
+                return index
+            except ValueError:
+                # id does not exist
+                return "id_doesn't_exist"
 
     def does_filter_match(self, list_filter, index):
         # check if the filter is contained in any substring of first/last name
@@ -72,16 +67,29 @@ class Validator:
     # validates that the new_id is still unique
     def modify_person(self, person_id, new_id, new_first, new_last):
 
+        # need to check if person_id is valid and exists
+        # need to check if new id is valid and doesn't exist
+
         index = self.does_id_exist(person_id)
-        if index != -1:
-            if self.update_person(index, new_id, new_first, new_last):
+        new_id_code = self.does_id_exist(new_id)
+
+        # if there is an error with the id passed in the body of the request, return error code
+        if isinstance(new_id_code, str):
+            return new_id_code
+
+        # if there is an error with the person_id passed in via the URL, return error code
+        if isinstance(index, str):
+            return index
+
+        elif isinstance(index, int):
+            if self.update_person(index, int(new_id), new_first, new_last):
                 return True
             else:
-                # if we reach this point, given id was not unique
-                return False
+                # if we reach this point, given id in the request body is not unique
+                return "id_exists"
         else:
             # if we reach this point, person that were trying to modify doesn't exist
-            return False
+            return "id_doesn't_exist"
 
     def update_person(self, index, new_id, new_first, new_last):
         # grabbing all the current information for the person
@@ -94,7 +102,7 @@ class Validator:
             if new_id in self.id_list:
                 return False
             else:
-                self.person_list[index].set_person_id(int(new_id))
+                self.person_list[index].set_person_id(new_id)
                 self.id_list[index] = new_id
 
         if self.did_first_name_change(first_name, new_first):
@@ -106,7 +114,7 @@ class Validator:
         return True
 
     def did_id_change(self, person_id, new_id):
-        if str(person_id) == new_id or new_id == "":
+        if person_id == new_id:
             return False
         else:
             return True
@@ -123,21 +131,28 @@ class Validator:
         else:
             return True
 
+
 def main():
-    p = Person(1, "a", "b")
-    p1 = Person(2, "c", "a")
+    p = Person(1, "devin", "kadillak")
+    p1 = Person(2, "steve", "joe")
     v = Validator()
     v.add_person(p)
     v.add_person(p1)
+    # print(v.person_list)
+    # print(v.id_list)
+    # print(v.modify_person(1, 0, "mister", "kadillak"))
+    # print(v.person_list)
+
+    # print(isinstance("id", str))
+    # print(isinstance(True, str))
+    # v.remove_person(0)
+    # print(v.person_list)
+    #
+    # print(v.id_list)
+
+    # print(v.does_id_exist("hello"))
+    print(v.modify_person(1, 3, "devin", "stinkerhead"))
     print(v.person_list)
-    print(v.modify_person(1, 0, "mister", "kadillak"))
-
-    print(v.person_list)
-
-    print(isinstance("id", str))
-    print(isinstance(True, str))
-
-
 
 
 if __name__ == '__main__':
--- project/app.py
+++ project/app.py
@@ -9,8 +9,8 @@ app = Flask('project')
 # instance variable which will store person objects
 personStorage = Validator()
 
-
-@app.route('/person/', methods=['GET'])
+# how do I make sure only ?filter=name is used, not ?donkey=name
+@app.route('/person', methods=['GET'])
 def get_all_people():
     person_filter = request.args.get('filter')
     if person_filter is None:
@@ -33,32 +33,45 @@ def create_person():
         so I need to check if the return is of boolean Type and equals True
     '''
     if type(call_status) == bool and call_status:
-        p = Person(data['id'], data['first_name'], data['last_name'])
-        if personStorage.add_person(p):
-            return Response("Successfully added person!", status=201)
-        else:
-            return Response("ERROR: person already exists", status=400)
+        return generate_post_response(data)
     else:
         return generate_error_response(call_status)
 
 
+def generate_post_response(data):
+    person_id = personStorage.does_id_exist(data['id'])
+    if isinstance(person_id, str) and person_id != "id_doesn't_exist":
+        return generate_error_response(person_id)
+    else:
+        p = Person(data['id'], data['first_name'], data['last_name'])
+        personStorage.add_person(p)
+        return Response("Successfully added person!", status=201)
+
+
 @app.route('/person/<person_id>', methods=['DELETE'])
 def delete_person(person_id):
-    index = personStorage.does_id_exist(person_id)
-    if index != -1:
-        personStorage.person_list.remove(personStorage.person_list[index])
-        return Response("Deleted person with id: "+person_id, status=200)
+    call_status = personStorage.does_id_exist(person_id)
+    if isinstance(call_status, str):
+        return generate_error_response(call_status)
     else:
-        return Response("ERROR: person with id "+person_id+" does not exist", status=400)
+        # if we reach this point, call_status will contain the index of the person to delete
+        # for clarity call this index
+        index = call_status
+
+        # updating person list and id list
+        personStorage.remove_person(index)
+        return Response("Deleted person with id: "+person_id, status=200)
 
 
+# fix this not generating error code when id doesn't exist
 @app.route('/person/<person_id>', methods=['GET'])
 def get_person_by_id(person_id):
     index = personStorage.does_id_exist(person_id)
-    if index != -1:
-        return Response(json.dumps({"person": str(personStorage.person_list[index])}), status=200)
+    if isinstance(index, str):
+        # index will be an error code if does_id_exist fails
+        return generate_error_response(index)
     else:
-        return Response("ERROR: person with id " + person_id + " does not exist", status=400)
+        return Response(json.dumps({"person": str(personStorage.person_list[index])}), status=200)
 
 
 @app.route('/person/<person_id>', methods=['PUT'])
@@ -75,10 +88,12 @@ def edit_person_by_id(person_id):
         new_first = data['first_name']
         new_last = data['last_name']
 
-        if personStorage.modify_person(person_id, new_id, new_first, new_last):
-            return Response("Person with id " + person_id + " was successfully modified", status=200)
+        modify_status = personStorage.modify_person(person_id, new_id, new_first, new_last)
+
+        if isinstance(modify_status, str):
+            return generate_error_response(modify_status)
         else:
-            return Response("ERROR: person with id " + person_id + " does not exist", status=400)
+            return Response("Person with id " + person_id + " was successfully modified", status=200)
 
     else:
         return generate_error_response(call_status)
@@ -113,11 +128,15 @@ def generate_error_response(call_status):
     elif call_status == "last":
         return Response("Error: 'last_name' field is not present in request body", status=400)
     elif call_status == "str_id":
-        return Response("Error: id must be an integer, the give id was a string", status=400)
+        return Response("Error: id must be an integer, the given id was a string", status=400)
     elif call_status == "float_id":
         return Response("Error: id must be an integer, given id was a float", status=400)
     elif call_status == "inv_id_range":
         return Response("Error: id must be an integer >= 1", status=400)
+    elif call_status == "id_exists":
+        return Response("Error: the given id in the body of the request is assigned to another person", status=400)
+    elif call_status == "id_doesn't_exist":
+        return Response("Error: a person does not exist with the given id", status=400)
 
 
 
